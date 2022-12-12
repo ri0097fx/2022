d2d2da6fbee5c7d9be543e6dadf5f208c35fb460
ri0097fx <ri0097fx@ed.ritsumei.ac.jp>
2022-12-12 12:55:09 +0900

    first commit

--- utils/utils.py
+++ utils/utils.py
@@ -13,20 +13,21 @@ import os
 from torchvision import transforms
 
 
-transform = transforms.Compose([transforms.ToPILImage(),
-                                transforms.Resize((256, 256)),
-                                transforms.ToTensor(),
-                                transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))
-                                ])
 
-def Make_dataset(path, batch_size=16):
+
+def Make_dataset(path, batch_size=16,img_size=28):
+    transform = transforms.Compose([transforms.ToPILImage(),
+                                    transforms.Resize((img_size, img_size)),
+                                    transforms.ToTensor(),
+                                    transforms.Normalize((0.5,),(0.5,))
+                                    ])
     data = []
     labels = []
     files = glob.glob(os.path.join(path,'*','*.*'))
     classes = os.listdir(path)
     for file in files:
         _cls = file.split('/')[-2]
-        img = cv2.imread(file)
+        img = cv2.imread(file,0)
         img = transform(img)
         data.append(img.unsqueeze(0))
         labels.append(classes.index(_cls))
